913158b9ba8ae4fcf4b3964862d7b0a7603367e8
Loc Nguyen <nguypham@oregonstate.edu>
2022-01-21 00:33:47 -0800

    Demo completed
    Demo completed

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+const axios = require('axios')
 //Loads the handlebars module
 const { engine } = require('express-handlebars');
+const getCoffee = async () => {
+  const baseURL = 'https://api.sampleapis.com/coffee/hot';
+  try {
+    const resp = await axios.get(baseURL);
+    console.log(resp.data);
+    return JSON.stringify(resp.data);
+  } catch(err) {
+    console.error(err);
+  }
+}
+
 //Sets our app to use the handlebars engine
 app.set('view engine', 'hbs');
 //Sets handlebars configurations
@@ -15,10 +27,8 @@ extname: 'hbs',
 defaultLayout: 'index'
 }));
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-
-
-res.render('main');
+app.get('/', async (req, res) => {
+res.render('main', {coffee: await getCoffee()});
 });
 
 app.listen(port, () => console.log(`App listening to port ${port}`))
